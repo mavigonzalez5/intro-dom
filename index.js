700824bab89e8ab73210bbae9de3a036fcf835be
Maria Virginia Gonzalez <mvirginiagonzalez5@gmail.com>
2024-04-11 17:05:50 +0200

    ejercicio 3 sin terminar

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function change() {
     // add red squares to the HTML when the button is clicked
+    let titleElement = document.querySelector("#titleIdName");
+    titleElement.appendChild(".redSquare");
 }
 
 
